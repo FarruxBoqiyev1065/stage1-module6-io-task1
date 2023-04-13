13fb1b8c082928544dd49e006ff52e3bbf14628a
macservis <fboqiyev1065@gmail.com>
2023-04-13 09:39:05 +0500

    stage1-module6-io-task1 updated

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.io;
 
-import java.io.File;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            StringBuilder str = new StringBuilder();
+            int i;
+            while ((i = fileInputStream.read()) != -1) {
+                str.append((char) i);
+            }
+            String[] split = str.toString().split("\n");
+            profile.setName(split[0].split(": ")[1]);
+            profile.setAge(Integer.valueOf(split[1].split(": ")[1]));
+            profile.setEmail(split[2].split(": ")[1]);
+            profile.setPhone(Long.valueOf(split[3].split(": ")[1]));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return profile;
     }
 }
